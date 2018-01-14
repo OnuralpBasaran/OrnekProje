e370031766cd148968aba6618b02480fa2bb7ccf
Onuralp Başaran <onur.alp@outlook.com>
2018-01-14 17:18:14 +0300

    Başlangıç, bitiş tarihi  ve malkodu validation kontrolleri ve arayüz düzenleme işlemleri.

--- OrnekProje/Form1.Designer.cs
+++ OrnekProje/Form1.Designer.cs
@@ -34,16 +34,19 @@
             this.txtBitisTarih = new System.Windows.Forms.TextBox();
             this.txtMalKodu = new System.Windows.Forms.TextBox();
             this.btnListele = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grdCtrEkstre)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // grdCtrEkstre
             // 
-            this.grdCtrEkstre.Location = new System.Drawing.Point(12, 39);
+            this.grdCtrEkstre.Location = new System.Drawing.Point(12, 59);
             this.grdCtrEkstre.MainView = this.gridView1;
             this.grdCtrEkstre.Name = "grdCtrEkstre";
-            this.grdCtrEkstre.Size = new System.Drawing.Size(843, 466);
+            this.grdCtrEkstre.Size = new System.Drawing.Size(843, 496);
             this.grdCtrEkstre.TabIndex = 0;
             this.grdCtrEkstre.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -56,7 +59,7 @@
             // txtBaslangicTarih
             // 
             this.txtBaslangicTarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBaslangicTarih.Location = new System.Drawing.Point(12, 10);
+            this.txtBaslangicTarih.Location = new System.Drawing.Point(12, 30);
             this.txtBaslangicTarih.Name = "txtBaslangicTarih";
             this.txtBaslangicTarih.Size = new System.Drawing.Size(100, 23);
             this.txtBaslangicTarih.TabIndex = 1;
@@ -65,7 +68,7 @@
             // txtBitisTarih
             // 
             this.txtBitisTarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBitisTarih.Location = new System.Drawing.Point(118, 10);
+            this.txtBitisTarih.Location = new System.Drawing.Point(118, 30);
             this.txtBitisTarih.Name = "txtBitisTarih";
             this.txtBitisTarih.Size = new System.Drawing.Size(100, 23);
             this.txtBitisTarih.TabIndex = 2;
@@ -74,25 +77,56 @@
             // txtMalKodu
             // 
             this.txtMalKodu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMalKodu.Location = new System.Drawing.Point(243, 10);
+            this.txtMalKodu.Location = new System.Drawing.Point(224, 30);
             this.txtMalKodu.Name = "txtMalKodu";
             this.txtMalKodu.Size = new System.Drawing.Size(237, 23);
             this.txtMalKodu.TabIndex = 3;
             // 
             // btnListele
             // 
-            this.btnListele.Location = new System.Drawing.Point(486, 10);
+            this.btnListele.Location = new System.Drawing.Point(467, 30);
             this.btnListele.Name = "btnListele";
             this.btnListele.Size = new System.Drawing.Size(75, 23);
             this.btnListele.TabIndex = 4;
             this.btnListele.Text = "Listele";
             this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Başlangıç Tarihi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Bitiş Tarihi:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mal Kodu:";
             // 
             // FrmStokEkstre
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(867, 515);
+            this.ClientSize = new System.Drawing.Size(867, 567);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnListele);
             this.Controls.Add(this.txtMalKodu);
             this.Controls.Add(this.txtBitisTarih);
@@ -116,6 +150,9 @@
         private System.Windows.Forms.TextBox txtBitisTarih;
         private System.Windows.Forms.TextBox txtMalKodu;
         private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- OrnekProje/Form1.cs
+++ OrnekProje/Form1.cs
@@ -16,5 +16,35 @@ namespace OrnekProje
         {
             InitializeComponent();
         }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int baslangicTarih = 0;
+                int bitisTarihi = 0;
+                string malKodu = "";
+
+                if ((txtBaslangicTarih.Text != "" && txtBitisTarih.Text != "") || txtMalKodu.Text != "")
+                {
+                    if (txtBaslangicTarih.Text != "")
+                        baslangicTarih = Convert.ToInt32(Convert.ToDateTime(txtBaslangicTarih.Text).ToOADate());
+
+                    if (txtBitisTarih.Text != "")
+                        bitisTarihi = Convert.ToInt32(Convert.ToDateTime(txtBitisTarih.Text).ToOADate());
+
+                    malKodu = txtMalKodu.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Başlangıç Ve Bitiş Tarihlerini Giriniz.");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bir Hata Oluştu.");
+            }
+        }
     }
 }
