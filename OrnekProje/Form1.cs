c14642b2a89de239149ae625797a9d5869bc67a5
Onuralp Başaran <onur.alp@outlook.com>
2018-01-14 18:32:23 +0300

    Stored procedure kullanılarak devexpress gird doldurulmuştur.

--- OrnekProje/Form1.cs
+++ OrnekProje/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace OrnekProje
 {
@@ -19,6 +21,15 @@ namespace OrnekProje
 
         private void btnListele_Click(object sender, EventArgs e)
         {
+            //Connection String windows authentication olarak oluşturuldu.
+            string connString = "Server=.;Database=Test;Trusted_Connection=True;";
+            
+            //Yeni bir connection oluşturulup connection string olarak yukardaki değişken atandı.
+            SqlConnection conn = new SqlConnection(connString);
+
+            //Yeni Bir sql command oluşturulup değerleri ilerleyen satırlarda doldurulmuştur.
+            SqlCommand cmd = new SqlCommand();
+
             try
             {
 
@@ -26,24 +37,61 @@ namespace OrnekProje
                 int bitisTarihi = 0;
                 string malKodu = "";
 
+                // Başlangıç ve bitiş tarihi seçilip malkodu veya adı seçilmeme durumu ve tarihlerin seçilmeyip sadece mal kodu veya adı seçilmesi yine aynı alanda kontrol edilir.
                 if ((txtBaslangicTarih.Text != "" && txtBitisTarih.Text != "") || txtMalKodu.Text != "")
                 {
+                    //bu alan eğer txtbaslangicTarihi boş değilse önce datetime daha sonra int değerine dönüştürülür.
                     if (txtBaslangicTarih.Text != "")
                         baslangicTarih = Convert.ToInt32(Convert.ToDateTime(txtBaslangicTarih.Text).ToOADate());
 
+                    //bu alan eğer txtBitisTarih boş değilse önce datetime daha sonra int değerine dönüştürülür.
                     if (txtBitisTarih.Text != "")
                         bitisTarihi = Convert.ToInt32(Convert.ToDateTime(txtBitisTarih.Text).ToOADate());
 
                     malKodu = txtMalKodu.Text;
+
+
+                    DataTable dt = new DataTable();
+
+                    //Yukarıda oluşturduğumuz commandı özelliklerini ve stored procedure ismini vermekteyiz.
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "sp_StokEkstre";
+
+                    //Bu if bie connection açıkmı değilmi kontrol ediyor kapalıysa yeni bir connection oluşturmaktadır.
+                    if (conn.State == ConnectionState.Closed)
+                        conn.Open();
+
+                    //Stored Procedure Icın gerekli olan paremetreleri buarada doldurulmaktadır.
+                    cmd.Parameters.AddWithValue("@BaslangicTarihi", baslangicTarih);
+                    cmd.Parameters.AddWithValue("@BitisTarihi", bitisTarihi);
+                    cmd.Parameters.AddWithValue("@MalKodu", malKodu);
+                    cmd.Parameters.AddWithValue("@MalAdi", malKodu);
+
+                    
+                  
+                    //Data table (dt) yi dataadapter ile doldurup devexpress gridview a datasource olarak verilmektedir.
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+
+                    grdCtrEkstre.DataSource = dt;
+                    
                 }
                 else
                 {
                     MessageBox.Show("Lütfen Başlangıç Ve Bitiş Tarihlerini Giriniz.");
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 MessageBox.Show("Bir Hata Oluştu.");
+            }
+            finally
+            {
+                //Bu işlemlerde en sonunda hata olsa da olmasda yapılcak şeilde oluşturulmuştur.
+                //Burada connection kapatılıp silinmektedir.
+                conn.Close();
+                conn.Dispose();
             }
         }
     }
